cf2d904ed76470070d97907e097aec34767e955d
Lemon Soju <yckk980320@naver.com>
2021-12-31 14:52:15 +0900

    my first commit

---
+++ src/main/java/hello/lemon_soju/LemonSojuApplication.java
@@ -0,0 +1,13 @@
+package hello.lemon_soju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemonSojuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemonSojuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/lemon_soju/LemonSojuApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.lemon_soju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemonSojuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
